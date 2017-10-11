337877b693b6e3f708db3b86b592f37ea1ab6ece
ChocolateTunder <32591685+ChocolateTunder@users.noreply.github.com>
2017-10-12 08:33:55 +1000

    Add files via upload

--- Tank.cs
+++ Tank.cs
@@ -17,7 +17,67 @@ namespace TankBattle
 
         public static void CreateLine(int[,] graphic, int X1, int Y1, int X2, int Y2)
         {
-            throw new NotImplementedException();
+            int dx = X2 - X1;
+            int dy = Y2 - Y1;
+
+            // Determine how steep the line is
+            bool isSteep;
+            if (Math.Abs(dy) > Math.Abs(dx)) {
+                isSteep = true;
+            } else {
+                isSteep = false;
+            }
+
+            // Rotate line if needed
+            if (isSteep) {
+                int tempX = X1;
+                X1 = Y1;
+                Y1 = tempX;
+
+                tempX = X2;
+                X2 = Y2;
+                Y2 = tempX;
+            }
+
+            // Swap start and end points if necessary
+            if (X1 > X2) {
+                int tempX = X1;
+                X1 = X2;
+                X2 = tempX;
+
+                int tempY = Y1;
+                Y1 = Y2;
+                Y2 = tempY;
+            }
+
+            // Recalculate deltas
+            dx = X2 - X1;
+            dy = Y2 - Y1;
+
+            // Calculate error 
+            int error = dx / 2;
+            int yStep;
+            if (Y1 < Y2) {
+                yStep = 1;
+            } else {
+                yStep = -1;
+            }
+
+            // Iterate to generate the line
+            int y = Y1;
+            for (int x = X1; x < X2 + 1; x++) {
+                if (isSteep) {
+                    graphic [y, x] = 1;
+                } else {
+                    graphic [x, y] = 1;
+                }
+
+                error = error - Math.Abs(dy);
+                if (error < 0) {
+                    y = y + yStep;
+                    error = error + dx;
+                }
+            }
         }
 
         public Bitmap CreateTankBMP(Color tankColour, float angle)
@@ -75,6 +135,26 @@ namespace TankBattle
 
         public static Tank CreateTank(int tankNumber)
         {
+            //TODO Add if statement to select tank type
+            return new BasicTank();
+        }
+    }
+    //TODO Add 5 more tank types
+    public class BasicTank : Tank {
+        //Creation of the default tank
+        public override void ActivateWeapon(int weapon, PlayerTank playerTank, Gameplay currentGame) {
+            throw new NotImplementedException();
+        }
+
+        public override int [,] DisplayTank(float angle) {
+            throw new NotImplementedException();
+        }
+
+        public override int GetTankHealth() {
+            return 100;
+        }
+
+        public override string [] GetWeapons() {
             throw new NotImplementedException();
         }
     }
